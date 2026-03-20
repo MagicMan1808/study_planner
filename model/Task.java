7bbe47e8ffe61752780acc0d5b2cd0ad2ad002e0
Yousef Mahmuod El-Jarousha <yousef.mahmuod.el-jarousha@stud.uni-hannover.de>
2026-03-20 20:10:06 +0100

    Task.java ready

--- model/Task.java
+++ model/Task.java
@@ -1,3 +1,32 @@
+import java.time.LocalDate;
+
 public class Task {
     
+    private String name;
+    private LocalDate deadline;
+    private int difficulty;
+    private int estimatedHours;
+
+    public Task(String name, LocalDate deadline, int difficulty, int estimatedHours) {
+        this.name = name;
+        this.deadline = deadline;
+        this.difficulty = difficulty;
+        this.estimatedHours = estimatedHours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public int getEstimatedHours() {
+        return estimatedHours;
+    }
 }
