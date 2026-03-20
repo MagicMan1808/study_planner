09f418d89ec599292e5de4da0eaaf7a63d3557fa
Yousef Mahmuod El-Jarousha <yousef.mahmuod.el-jarousha@stud.uni-hannover.de>
2026-03-20 22:31:49 +0100

    Algorithmus zur berechnung der lernzeit pro Tag (noch Basisvariante)

--- main/main.java
+++ main/main.java
@@ -6,7 +6,14 @@ package main;
 
 import model.Task;
 import model.StudySession;
+import planner.StudyPlannerAlgorithm;
+
+
 import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+
+
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +22,14 @@ public class Main {
 
         StudySession session = new StudySession(LocalDate.of(2026, 1, 3), "Prog II", 5);
         System.out.println(session);
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Mathe", LocalDate.now().plusDays(5), 5, 10));
+        tasks.add(new Task("Prog", LocalDate.now().plusDays(3),  3, 7));
+
+        StudyPlannerAlgorithm algo = new StudyPlannerAlgorithm();
+        List<StudySession> plan = algo.generatePlan(tasks);
+
+        plan.forEach(System.out::println);
     }
 }
