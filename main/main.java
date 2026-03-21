dfb0b350a1c5fe8e99dcb2dcc27a8777dbc45762
Yousef Mahmuod El-Jarousha <yousef.mahmuod.el-jarousha@stud.uni-hannover.de>
2026-03-21 21:35:14 +0100

    StorageManager fertig

--- main/main.java
+++ main/main.java
@@ -7,7 +7,8 @@ package main;
 import model.Task;
 import model.StudySession;
 import planner.PlanningService;
-import planner.StudyPlannerAlgorithm;
+//import planner.StudyPlannerAlgorithm;
+import storage.StorageManager;
 
 
 import java.time.LocalDate;
@@ -27,16 +28,40 @@ public class Main {
         //System.out.println(session);
 
         // StudyPlannerAlgorithm.java test
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Mathe", LocalDate.now().plusDays(3), 5, 10));
-        tasks.add(new Task("Prog", LocalDate.now().plusDays(7),  3, 8));
-        tasks.add(new Task("GBS", LocalDate.now().plusDays(10), 2, 5));
+        //List<Task> tasks = new ArrayList<>();
+        //tasks.add(new Task("Mathe", LocalDate.now().plusDays(3), 5, 10));
+        //tasks.add(new Task("Prog", LocalDate.now().plusDays(7),  3, 8));
+        //tasks.add(new Task("GBS", LocalDate.now().plusDays(10), 2, 5));
 
+        // Ohne PlanningService.java
+        //StudyPlannerAlgorithm algo = new StudyPlannerAlgorithm();
+        //List<StudySession> plan = algo.generatePlan(tasks);
+
+        StorageManager storage = new StorageManager();
+
+        // 1. Tasks laden
+        List<Task> tasks = storage.loadTasks();
+
+        // 2. Falls keine vorhanden -> Demo-Dateien erstellen
+        if (tasks.isEmpty()) {
+            tasks = new ArrayList<>();
+            tasks.add(new Task("Mathe", LocalDate.now().plusDays(3), 5, 10));
+            tasks.add(new Task("Prog", LocalDate.now().plusDays(7),  3, 8));
+            tasks.add(new Task("GBS", LocalDate.now().plusDays(10), 2, 5));
+        }
+
+        // 3. Plan erstellen
         PlanningService service = new PlanningService();
         List<StudySession> plan = service.createPlan(tasks);
 
+        // 4. Plan ausgeben
+        System.out.println("\n--- STUDY PLAN ---");
         for (StudySession session : plan) {
             System.out.println(session);
         }
+
+
+        // 5. Tasks speichern
+        storage.saveTasks(tasks);
     }
 }
--- storage/StorageManager.java
+++ storage/StorageManager.java
@@ -1,5 +1,51 @@
 package storage;
 
+import model.Task;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageManager {
     
+    private static final String FILE_PATH = "storage/data.json";
+
+    public void saveTasks(List<Task> tasks) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            
+            for (Task task : tasks) {
+                String line = task.getName() + ";" + task.getDeadline() + ";" + task.getDifficulty() + ";" + task.getEstimatedHours();
+
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Task> loadTasks() {
+        List<Task> tasks = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(";");
+
+                String name = parts[0];
+                java.time.LocalDate deadline = java.time.LocalDate.parse(parts[1]);
+                int difficulty = Integer.parseInt(parts[2]);
+                int hours = Integer.parseInt(parts[3]);
+
+                tasks.add(new Task(name, deadline, difficulty, hours));
+            }
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tasks;
+    }
 }
